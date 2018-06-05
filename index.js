fc27dabb43e3937fe4c5d856e10cb2692517826a
Jesse McReady <jessemcready@gmail.com>
2018-06-05 10:35:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for(let i = 0; i < cart.length; i++){
-    if(cart[i] === item){
+    if(cart[i].itemName === item){
       cart.splice(i, 1);
       return cart;
     }
