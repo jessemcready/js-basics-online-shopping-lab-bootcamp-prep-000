8791e62f64cc3f6c305f73087b2cc317b69ef043
Jesse McReady <jessemcready@gmail.com>
2018-06-05 10:36:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,4 +60,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(cardNumber === null){
+    return "Sorry, we don't have a credit card on file for you.";
+  }
+  let total = total();
+  
 }
