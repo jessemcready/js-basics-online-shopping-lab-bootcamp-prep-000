670cf77f8ce2d9653ba496e3defc65c3090bf433
Jesse McReady <jessemcready@gmail.com>
2018-06-05 10:31:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ function viewCart() {
   let cartItems = `In your cart, you have `;
   for(var i = 0; i < cart.length; i++){
     if(i === (cart.length - 1)){
-      cartItems += `${cart[i].itemName} at $${cart[i].itemPrice}.`;
+      if(cart.length > 1){
+        cartItems += `and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+      }else {
+        cartItems += `${cart[i].itemName} at $${cart[i].itemPrice}.`;
+      }
     } else{
       cartItems += `${cart[i].itemName} at $${cart[i].itemPrice}, `;
     }
